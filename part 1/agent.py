f91f31c58918bda19c973d751d715811f3873fe0
Jessica Gomes <jessicagomes@Jessicas-MacBook-Pro.local>
2019-04-16 15:12:40 -0500

    reworked part 1

--- part 1/agent.py
+++ part 1/agent.py
@@ -12,6 +12,8 @@ class Agent:
         self.gamma = gamma
         self.first = True
         self.track_actions = []
+        self.s = None
+        self.a = None
 
         # Create the Q and N Table to work with
         self.Q = utils.create_q_table()
@@ -60,19 +62,19 @@ class Agent:
         else:
             adjoining_walls_y = 0
         ## food direction left or right
-        if (food_y < snake_y):
-            food_dir_y = 1
-        elif (food_y > snake_y):
-            food_dir_y = 2
-        else:
-            food_dir_y = 0
-        ## food direction up or down
         if (food_x < snake_x):
             food_dir_x = 1
         elif (food_x > snake_x):
             food_dir_x = 2
         else:
             food_dir_x = 0
+        ## food direction up or down
+        if (food_y < snake_y):
+            food_dir_y = 1
+        elif (food_y > snake_y):
+            food_dir_y = 2
+        else:
+            food_dir_y = 0
         ## is body on top
         new_x = snake_x
         new_y = snake_y - 40
@@ -103,49 +105,49 @@ class Agent:
              adjoining_body_right = 0
         return (adjoining_walls_x, adjoining_walls_y, food_dir_x, food_dir_y, adjoining_body_top, adjoining_body_bottom, adjoining_body_left, adjoining_body_right)
 
-    # TAKEN FROM MOVE(...) IN SNAKE.PY
-    def check_alive(self, state, action):
-        track_head = None
-        if len(state[2]) == 1:
-            track_head = state[2][0]
-        state[2].append((state[0], state[1]))
-        if action == 0:
-            state[1] -= 40
-        elif action == 1:
-            state[1] += 40
-        elif action == 2:
-            state[0] -= 40
-        elif action == 3:
-            state[0] += 40
+    # # TAKEN FROM MOVE(...) IN SNAKE.PY
+    # def check_alive(self, state, action):
+    #     track_head = None
+    #     if len(state[2]) == 1:
+    #         track_head = state[2][0]
+    #     state[2].append((state[0], state[1]))
+    #     if action == 0:
+    #         state[1] -= 40
+    #     elif action == 1:
+    #         state[1] += 40
+    #     elif action == 2:
+    #         state[0] -= 40
+    #     elif action == 3:
+    #         state[0] += 40
 
-        # check body length less than points
-        # if len(state[2]) > points:
-        del(state[2][0])
+    #     # check body length less than points
+    #     # if len(state[2]) > points:
+    #     # del(state[2][0])
 
-        if len(state[2]) >= 1:
-            for seg in state[2]:
-                if state[0] == seg[0] and state[1] == seg[1]:
-                    return False
+    #     if len(state[2]) >= 1:
+    #         for seg in state[2]:
+    #             if state[0] == seg[0] and state[1] == seg[1]:
+    #                 return False
 
-        if len(state[2]) == 1:
-            if track_head == (state[0], state[1]):
-                return False
+    #     if len(state[2]) == 1:
+    #         if track_head == (state[0], state[1]):
+    #             return False
 
-        if (state[0] < 40 or state[1] < 40 or
-            state[0] + 40 > 520 or state[1] + 40 > 520):
-            return False
+    #     if (state[0] < 40 or state[1] < 40 or
+    #         state[0] + 40 > 520 or state[1] + 40 > 520):
+    #         return False
 
-        return True
+    #     return True
 
-    def compute_q(self, state, action):
-        curr_state = self.makeState(state)
+    def compute_q(self, prev_state, prev_action, state, dead, points):
+        prev = self.makeState(prev_state)
         #a
-        alpha = self.C / (self.C + self.N[curr_state[0]][curr_state[1]][curr_state[2]][curr_state[3]][curr_state[4]][curr_state[5]][curr_state[6]][curr_state[7]][action])
+        alpha = self.C / (self.C + self.N[prev[0]][prev[1]][prev[2]][prev[3]][prev[4]][prev[5]][prev[6]][prev[7]][prev_action])
 
         #R(s)
-        if (state[0] == state[3] and state[1] == state[4]):
+        if points - self.points > 0:
             R = 1
-        elif not self.check_alive(state, action):
+        elif dead:
             R = -1
         else:
             R = -0.1
@@ -161,7 +163,7 @@ class Agent:
         a_prime = max(q_U, q_D, q_L, q_R)
 
         #Q_val
-        Q_val = self.Q[curr_state[0]][curr_state[1]][curr_state[2]][curr_state[3]][curr_state[4]][curr_state[5]][curr_state[6]][curr_state[7]][action]
+        Q_val = self.Q[prev[0]][prev[1]][prev[2]][prev[3]][prev[4]][prev[5]][prev[6]][prev[7]][prev_action]
         return ( Q_val + alpha * (R + self.gamma*a_prime - Q_val) )
 
 
@@ -177,8 +179,22 @@ class Agent:
         Tips: you need to discretize the state to the state space defined on the webpage first.
         (Note that [adjoining_wall_x=0, adjoining_wall_y=0] is also the case when snake runs out of the 480x480 board)
 
-        '''       
+        '''   
         curr_state = self.makeState(state)
+
+        if dead:
+            # print("GAME OVER")
+            # print(state[0], state[1])
+            # print("POINTS: ", points)
+            # print("ACTIONS: ", self.track_actions)
+            prev_state = self.makeState(self.s)
+            self.Q[prev_state[0]][prev_state[1]][prev_state[2]][prev_state[3]][prev_state[4]][prev_state[5]][prev_state[6]][prev_state[7]][self.a] = self.compute_q(self.s, self.a, state, dead, points)
+            self.reset()
+            return 
+
+        if (self.s != None and self.a != None):
+            self.compute_q(self.s, self.a, state, dead, points)
+
 
         ## choosing which action to pick
         utility = np.zeros(4)
@@ -189,64 +205,14 @@ class Agent:
                 utility[i] = 1
             else:
                 utility[i] = Q_val
-        idxs = utility[::-1]
-        action = np.argmax(idxs)
-        action = 3 - action
+        idxs = np.argwhere(utility == np.amax(utility))
+        action = idxs[len(idxs)-1]
         self.track_actions.append(action)
-
-        # idxs = np.argwhere(utility == np.amax(utility))
-        # action = idxs[len(idxs)-1]
 
         ## updating N(s,a)
         self.N[curr_state[0]][curr_state[1]][curr_state[2]][curr_state[3]][curr_state[4]][curr_state[5]][curr_state[6]][curr_state[7]][action] += 1
 
-        new_q = self.compute_q(state, action)
-        if not self.first:
-            self.Q[curr_state[0]][curr_state[1]][curr_state[2]][curr_state[3]][curr_state[4]][curr_state[5]][curr_state[6]][curr_state[7]][action] = new_q
-        else:
-            self.first = False
-
-        """
-        ## Finding Q(s', a')
-        ## s' 
-        if (action == 0):
-            new_snake_y = state[1] - 40
-            s_prime = self.makeState([state[0], new_snake_y, state[2], state[3], state[4]])
-        if (action == 1):
-            new_snake_y = state[1] + 40
-            s_prime = self.makeState([state[0], new_snake_y, state[2], state[3], state[4]])
-        if (action == 2):
-            new_snake_x = state[0] - 40
-            s_prime = self.makeState([new_snake_x, state[1], state[2], state[3], state[4]])
-        if (action == 3):
-            new_snake_x = state[0] + 40
-            s_prime = self.makeState([new_snake_x, state[1], state[2], state[3], state[4]])
-
-        ## max Q(s', a')
-        next_utility = np.zeros(4)
-        for i in range(4):
-            N_val = self.N[s_prime[0]][s_prime[1]][s_prime[2]][s_prime[3]][s_prime[4]][s_prime[5]][s_prime[6]][s_prime[7]][i]
-            Q_val = self.Q[s_prime[0]][s_prime[1]][s_prime[2]][s_prime[3]][s_prime[4]][s_prime[5]][s_prime[6]][s_prime[7]][i]
-            if N_val < self.Ne:
-                next_utility[i] = 1
-            else:
-                next_utility[i] = Q_val
-
-        idxs = next_utility[::-1]
-        a_prime = np.argmax(idxs)
-        a_prime = 3 - a_prime
-        # idxs = np.argwhere(next_utility == np.amax(next_utility))
-        # a_prime = idxs[len(idxs)-1]
-        """
-
-        # print(self.Q[curr_state[0]][curr_state[1]][curr_state[2]][curr_state[3]][curr_state[4]][curr_state[5]][curr_state[6]][curr_state[7]][action])
-        if dead:
-            print("GAME OVER")
-            print(state[0], state[1])
-            print("POINTS: ", points)
-            print("ACTIONS: ", self.track_actions)
-            self.Q[curr_state[0]][curr_state[1]][curr_state[2]][curr_state[3]][curr_state[4]][curr_state[5]][curr_state[6]][curr_state[7]][action] = self.compute_q(state, action, points)
-            self.reset()
-            return 
-
+        self.s = state
+        self.a = action
+        self.points = points
         return action
--- part 1/test.py
+++ part 1/test.py
@@ -2,9 +2,18 @@ import numpy as np
 
 def main():
 	arr = np.array([3,3,3,2])
-	idxs = np.argwhere(arr == np.amax(arr))
-	final_idx = idxs[len(idxs)-1]
-	print(final_idx)
+	# idxs = np.argwhere(arr == np.amax(arr))
+	# final_idx = idxs[len(idxs)-1]
+	arr = arr[::-1]
+	action = np.argmax(arr)
+	action = 3 - action
+	print(action)
+	(new_x, new_y) = (5, 5)
+	body = [(3,3), (4,4), (5,5), (6,6)]
+	if (new_x, new_y) in body:
+		print("in body")
+	else:
+		print("not in body")
 
 if __name__ == "__main__":
     main()
